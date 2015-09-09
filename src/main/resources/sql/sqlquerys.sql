8bc58181844b31ba442298ca4b723b5bf3b96d17
Mariano <mariano@mariano-Satellite-A665D>
2015-09-09 00:50:05 -0300

    se agrega archivo con las querys sql

---
+++ src/main/resources/sql/sqlquerys.sql
@@ -0,0 +1,14 @@
+
+select * from all_tables;
+
+SELECT * FROM estudio.Concesionario;
+SELECT * FROM estudio.Auto;
+
+desc estudio.Auto;
+
+SELECT  *
+FROM estudio.Auto A,
+estudio.Concesionario as C
+where A.idConcesionario = C.idConcesionario;
+
+
