d867273bb39adaba9e299f110f7ae4b2ad28657e
Mariano <mariano@mariano-Satellite-A665D>
2016-07-29 00:12:33 -0300

    se agregan querys de clientes personas y vendedores

--- src/main/resources/sql/sqlquerys.sql
+++ src/main/resources/sql/sqlquerys.sql
@@ -1,8 +1,13 @@
-
-select * from all_tables;
-
+SELECT * /*DISTINCT TABLE_NAME*/ 
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA='estudio';
+	AND TABLE_NAME IN ('Auto');
+        
 SELECT * FROM estudio.Concesionario;
 SELECT * FROM estudio.Auto;
+SELECT * FROM estudio.PERSONAS;
+SELECT * FROM estudio.CLIENTES;
+SELECT * FROM estudio.VENDEDORES;
 
 desc estudio.Auto;
 
@@ -11,4 +16,12 @@ FROM estudio.Auto A,
 estudio.Concesionario as C
 where A.idConcesionario = C.idConcesionario;
 
+SHOW CREATE TABLE estudio.Clientes;
 
+SHOW CREATE TABLE estudio.Vendedores;
+SHOW VARIABLES LIKE '%innodb%';
+SHOW TABLE STATUS;
+SELECT @@version;		
+select @@GLOBAL.sql_mode;
+
+DROP TABLE estudio.Personas;
