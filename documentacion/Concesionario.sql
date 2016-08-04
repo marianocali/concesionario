d0f559f9a7ae5ce81d460b39a2c965f2ea9f52d8
Mariano <marianocali@gmail.com>
2016-08-04 14:53:10 -0300

    correcion

--- documentacion/Concesionario.sql
+++ documentacion/Concesionario.sql
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * Author:  mariano
  * Created: 04/08/2016
@@ -11,6 +6,6 @@ CREATE TABLE estudio.CONCESIONARIO (
 	IDCONCESIONARIO BIGINT(20) NOT NULL AUTO_INCREMENT,
 	NOMBRE VARCHAR(50) ,
 	DIRECCION VARCHAR(50) ,
-	PRIMARY KEY (ID_PERSONA)
+	PRIMARY KEY (IDCONCESIONARIO)
 );
 
