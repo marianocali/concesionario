e995896681d79f8bed3e4219c84f05d335c742ea
Mariano <marianocali@gmail.com>
2016-08-10 17:58:54 -0300

    se agregan test de modificar y eliminar

--- src/test/java/com/educacionit/jpa/VendedorDaoTest.java
+++ src/test/java/com/educacionit/jpa/VendedorDaoTest.java
@@ -8,15 +8,19 @@ package com.educacionit.jpa;
 import com.concesionaria.dao.DaoFactory;
 import com.concesionaria.dao.VendedorDaoImpl;
 import com.concesionaria.domain.Concesionario;
+import com.concesionaria.domain.Persona;
 import com.concesionaria.domain.Vendedor;
 import java.util.Calendar;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 /**
  *
  * @author mariano
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class VendedorDaoTest {
 
     private static Long idInsertado;    //se usa para saber que idSe inserto, modificarlo, borrarlo
@@ -33,13 +37,13 @@ public class VendedorDaoTest {
     }
 
     @Test
-    public void testGuardar() {
+    public void test1Guardar() {
         fechaIngreso.set(2010, 02, 21);
-        
+
         String legajo = "2105";
         double sueldo = 25000;
         long maxIdConcesionario = DaoFactory.getConcesionarioDao().getMaxId();
-        
+
         Concesionario concesionario = DaoFactory.getConcesionarioDao().findById(maxIdConcesionario);
         Vendedor vendedor = new Vendedor("Hector", "Saldinger", "Burruchaga 357", fechaIngreso, legajo, sueldo, concesionario);
 
@@ -50,13 +54,47 @@ public class VendedorDaoTest {
     }
 
     @Test
-    public void testFindById() {
+    public void test2FindById() {
 
         Vendedor vendedor = (Vendedor) vendedorDao.buscarPorId(getIdInsertado());
 
         Assert.assertEquals(getIdInsertado(), Long.valueOf(vendedor.getIdPersona())); //se agrega el Long.valueOf porque sino no compila el test
     }
-    
-    
-    
+
+    @Test
+    public void test3Modificar() {
+
+        Vendedor vendedor = (Vendedor) vendedorDao.buscarPorId(getIdInsertado());
+        String nuevoNombre = "Miranda";
+        String nuevoApellido = "Rodriguez";
+        String nuevoLegajo = "1735";
+        double nuevoSueldo = 35800;
+        Calendar nuevaFecha = Calendar.getInstance();
+        nuevaFecha.add(1, 3);
+
+        vendedor.setNombre(nuevoNombre);
+        vendedor.setApellido(nuevoApellido);
+        vendedor.setLegajo(nuevoLegajo);
+        vendedor.setSueldo(nuevoSueldo);
+        vendedor.setFechaIngreso(nuevaFecha);
+
+        vendedorDao.modificar(vendedor);
+
+        Vendedor vendedor2 = (Vendedor) vendedorDao.buscarPorId(getIdInsertado());
+
+        Assert.assertEquals(vendedor2.getNombre(), nuevoNombre);
+        Assert.assertEquals(vendedor2.getApellido(), nuevoApellido);
+        Assert.assertEquals(vendedor2.getLegajo(), nuevoLegajo);
+        Assert.assertEquals(String.valueOf(vendedor2.getSueldo()), String.valueOf(nuevoSueldo));
+
+    }
+
+    @Test
+    public void test4Eliminar() {
+
+        vendedorDao.eliminar(getIdInsertado());
+        Assert.assertNull(vendedorDao.buscarPorId(getIdInsertado()));
+
+    }
+
 }
