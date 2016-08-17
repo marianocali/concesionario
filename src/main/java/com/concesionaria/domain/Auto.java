860073a455b362e9443fb1e0846e3116c5b81a79
Mariano <mariano@mariano-Satellite-A665D>
2016-08-17 09:25:43 -0300

    se mapea la clase auto con la tabla AUTOS en plural

--- src/main/java/com/concesionaria/domain/Auto.java
+++ src/main/java/com/concesionaria/domain/Auto.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -17,6 +18,7 @@ import javax.persistence.TemporalType;
  * @author mariano
  */
 @Entity
+@Table(name = "AUTOS")
 public class Auto implements Serializable {
 
     @Id
