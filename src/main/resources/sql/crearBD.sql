5d493d3af6d2e7178c6b77ed313f3e2dc6cfa37f
Mariano <mariano@mariano-Satellite-A665D>
2016-08-17 09:24:55 -0300

    se crea un script para crear la BD

---
+++ src/main/resources/sql/crearBD.sql
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  mariano
+ * Created: 08/08/2016
+ */
+
+-- Creación de la Base de Datos estudio
+CREATE DATABASE estudio;
+-- Usar la Base de datos estudio
+USE estudio;
