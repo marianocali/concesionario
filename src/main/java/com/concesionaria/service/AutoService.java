b1965710d1ff1d2a9bccfa00cd8557e5a7e2100f
Mariano <mariano@mariano-Satellite-A665D>
2016-08-21 12:34:02 -0300

    se agrega informarAutosOrdenadosPorPrecio

--- src/main/java/com/concesionaria/service/AutoService.java
+++ src/main/java/com/concesionaria/service/AutoService.java
@@ -6,6 +6,7 @@
 package com.concesionaria.service;
 
 import com.concesionaria.dao.DaoFactory;
+import com.concesionaria.dao.GetEntityManagerFactory;
 import com.concesionaria.domain.Auto;
 import java.util.List;
 
@@ -14,19 +15,37 @@ import java.util.List;
  * @author mariano
  */
 public class AutoService {
-    
-    
-    public static void informarAutosMayorAPrecio(Double precio){
-        
-        List <Auto> listaAutos;
+
+    public static void informarAutosMayorAPrecio(Double precio) {
+
+        List<Auto> listaAutos;
         listaAutos = DaoFactory.getAutoDao().informarAutosMayorAPrecio(precio);
-        
+
         System.out.println("Datos de los autos");
         for (Auto auto : listaAutos) {
             auto.mostrarAuto(auto);
         }
 
         System.out.println("\n\n");
-        
+
+    }
+
+    public static void informarAutosOrdenadosPorPrecio() {
+
+        List<Auto> listaAutos;
+        listaAutos = DaoFactory.getAutoDao().informarAutosOrdenadosPorPrecio();
+
+        System.out.println();
+        System.out.println("Autos de todos los concesionarios ordenados por precio de mayor a menor");
+        for (Auto auto : listaAutos) {
+            auto.mostrarAuto(auto);
+        }
+
+        System.out.println("\n\n");
+    }
+    
+    public static void main(String[] args) {
+        AutoService.informarAutosOrdenadosPorPrecio();
+        GetEntityManagerFactory.getInstance().close();
     }
 }
